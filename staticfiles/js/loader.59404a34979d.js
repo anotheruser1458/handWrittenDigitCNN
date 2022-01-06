347c20a9cded457e3f2a7e9808ea60dc71800255
anotheruser1458 <maxwellpauly32@gmail.com>
2022-01-06 03:44:13 -1000

    staticfiles

---
+++ staticfiles/js/loader.59404a34979d.js
@@ -0,0 +1,6 @@
+loader = document.querySelector(".loader")
+
+function toggleLoader() {
+    if (loader.style.display == "none") loader.style.display = "block";
+    else loader.style.display = "none"
+}
